694ca85935d08a7effcfc8b75508f6bb81d88fe3
Miracle, Calvin B. <calvin.miracle@jefferson.kyschools.us>
2025-01-30 16:00:45 -0500

    Initial solution structure

---
+++ Src/GutenbergMetadataLookup/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GutenbergMetadataLookup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
